1cbfd384f78b535400e435d3144d7782f350999f
juliabialy <132374072+juliabialy@users.noreply.github.com>
2024-03-12 14:51:36 +0100

    Create cw.py

---
+++ cw.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import random
+
+lista1 = []
+for el in range(0,10):
+    lista1.append( random.randint(0,100))
+
+dane1 = pd.Series(lista1)
+
+print(dane1)
+print (lista1)
+
+
+lista2 = [31,28,30,31]
+indeksy = ["styczen","luty","marzec","kwiecien"]
+dane2 = pd.Series(lista2,index=indeksy)
+print(dane2)
+
+#print(dane2.loc["styczeń"]) szuka po etykietach (styczen, luty...)
+print(dane2.iloc[0:2]) # szuka po indeksach ;        (iloc[:3]-> wszystkie el do 3)
+
+
+
+
+# krotka = (13,21,5,5,43)
+# dane3 = pd.Series(krotka)
+# print(f"Krotka: {dane3}")
+
+
+
+# slownik = {"styczen":31,"luty":28,"marzec":30,"kwiecień":31}
+# dane4 = pd.Series(slownik)
+# print( {dane4})
+
+
+# print(dane[dane>6][dane<70])
+# print(dane.to_string()) <- zobaczyszmy wszystkie dane
